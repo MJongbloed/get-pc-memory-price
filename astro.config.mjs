5c1c2e472b7374fd057c0412b023aace0fafcd1e
Michel Jongbloed <micheljongbloed@MBP-van-Michel.fritz.box>
2025-04-09 15:36:31 +0200

    Added the first blog to the website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://pcmemoryfinder.com', // Add your site URL here
+  integrations: [sitemap()],
   redirects: {
     '/Computer-Memory-Chart': '/'
   }
