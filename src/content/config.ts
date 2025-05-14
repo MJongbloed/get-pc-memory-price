c1716b490a08a91f83add1b461908a43ce767654
MJongbloed <jongbloed@accentio.nl>
2025-05-14 16:17:32 +0200

    Fixed an issue with the meta tags of the blog posts.

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,8 +5,11 @@ const blog = defineCollection({
     title: z.string(),
     description: z.string(),
     pubDate: z.string(),
+    author: z.string(),
     tags: z.array(z.string()),
+    keywords: z.array(z.string()),
     image: z.string().optional(),
+    canonicalUrl: z.string(),
   }),
 });
 
